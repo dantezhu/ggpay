d5fcd42151f9cee3c38e98041421045dfc0e39b0
dantezhu <dantezhu@qq.com>
2020-04-06 15:49:42 +0800

    版本升级

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="ggpay",
-    version="0.3.1",
+    version="0.3.2",
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     install_requires=['requests'],
     scripts=[],
